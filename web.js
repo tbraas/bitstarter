75bff10e5ab9449714101d875d05571178de23a1
Tommy Braas <tommy@anno1969.com>
2013-07-11 14:32:12 +1000

    Changed content served from hard coded string to 'index.html'

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buffer = fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+  }
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
