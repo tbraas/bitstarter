9e1d86724316a6097752c4b463fcfb1bbe040b2a
Tommy Braas <tommy@anno1969.com>
2013-08-08 16:46:34 +1000

    updated bootstrap to 3.0 RC1

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+$(document).ready(function () {
+   var email = "direct0r" + "@" + "anno1969" + "." + "com";
+   $('#email').html("<a " + "href='mailto:" + email + "'>" + email + "</a>")   
+});
+
